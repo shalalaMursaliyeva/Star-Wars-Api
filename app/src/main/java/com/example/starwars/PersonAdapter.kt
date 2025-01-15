29d823434a7da087bb5219b49fe1018278b58325
shalala.mursaliyeva <shalala.mursaliyeva@gmail.com>
2025-01-16 00:37:30 +0400

    items are shown in recyclerview

--- app/src/main/java/com/example/starwars/PersonAdapter.kt
+++ app/src/main/java/com/example/starwars/PersonAdapter.kt
@@ -1,19 +1,20 @@
 package com.example.starwars
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.starwars.databinding.FragmentPersonDetailBinding
+import com.example.starwars.databinding.ItemPersonBinding
 import com.example.starwars.model.data.Person
-
 
 class PersonAdapter(private val onItemClick: (Person) -> Unit) :
     ListAdapter<Person, PersonAdapter.PersonViewHolder>(DiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonViewHolder {
-        val binding = FragmentPersonDetailBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemPersonBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return PersonViewHolder(binding)
     }
 
@@ -22,10 +23,26 @@ class PersonAdapter(private val onItemClick: (Person) -> Unit) :
         person?.let { holder.bind(it, onItemClick) }
     }
 
-    class PersonViewHolder(private val binding: FragmentPersonDetailBinding) : RecyclerView.ViewHolder(binding.root) {
+    class PersonViewHolder(private val binding: ItemPersonBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(person: Person, onItemClick: (Person) -> Unit) {
             binding.nameTextView.text = person.name
-            itemView.setOnClickListener { onItemClick(person) }
+
+            binding.eyeColorBox.setBackgroundColor(getColorFromEyeColor(person.eyeColor))
+            binding.root.setOnClickListener { onItemClick(person) }
+        }
+
+        private fun getColorFromEyeColor(eyeColor: String?): Int {
+            return when (eyeColor?.lowercase()) {
+                "blue" -> Color.BLUE
+                "brown" -> Color.parseColor("#A52A2A")
+                "green" -> Color.GREEN
+                "yellow" -> Color.YELLOW
+                "red" -> Color.RED
+                "pink" -> Color.MAGENTA
+                "black" -> Color.BLACK
+                "white" -> Color.WHITE
+                else -> Color.LTGRAY
+            }
         }
     }
 
